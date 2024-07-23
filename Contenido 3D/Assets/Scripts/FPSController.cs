021b28b781125ad0ccf6fb4d7c76a6c187a2ae04
Arario64 <152214762+Arario64@users.noreply.github.com>
2024-07-23 13:41:10 -0600

    lol

---
+++ Contenido 3D/Assets/Scripts/FPSController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CharacterController))]
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Camera playercamra;
+    public float walkspeed = 6f;
+    public float runspeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+
+    public float lookspeed = 2f;
+    public float lookXLimit = 45f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0f;
+
+    public bool canMove = true;
+
+    CharacterController charactercontroller;
+    void Start()
+    {
+        charactercontroller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runspeed : walkspeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runspeed : walkspeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && charactercontroller.isGrounded) 
+        {
+            moveDirection.y = jumpPower;
+        
+        }
+    }
+}
